1898efdb9ef99d30e752b5371a05715d2fa69cf4
Anastasia <nastia.sharnenkova@gmail.com>
2019-03-27 12:12:13 +0200

    Add React component ModalWindow

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Score from './components/Score'
 import CardList from './components/CardList'
+import Modal from './components/Modal'
 import "./styles.css";
 
 function App(){
     return(
         <div class="app">
-            <CardList />
-            <Score value={0}/>
+            {/* <CardList />
+            <Score value={0}/> */}
+
+            <Modal />
         </div>
     )
 }
